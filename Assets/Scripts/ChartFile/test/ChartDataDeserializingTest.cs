fcbc1c26183c65053432ef3d3300e1e79cbd330b
FraMari495 <mirochacha_u_u_1106@yahoo.co.jp>
2023-12-13 04:29:08 +0900

    [modify] リフォーマット

--- Assets/Scripts/ChartFile/test/ChartDataDeserializingTest.cs
+++ Assets/Scripts/ChartFile/test/ChartDataDeserializingTest.cs
@@ -1,28 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
+using LitJson;
 using UnityEngine;
 
 namespace ChartFile.Test
 {
     /// <summary>
-    /// 作成したモデルクラスが正しく動作していることを確認するためのクラス
+    ///     作成したモデルクラスが正しく動作していることを確認するためのクラス
     /// </summary>
     public class ChartDataDeserializingTest : MonoBehaviour
     {
-        void Start()
+        private void Start()
         {
             // jsonファイルを読み込み
             var chartText = Resources.Load<TextAsset>("Songs/chart_sample/chart_sample");
-            
+
             // jsonデータをデシリアライズ
-            var chart = LitJson.JsonMapper.ToObject<ChartData>(chartText.text);
-            
+            var chart = JsonMapper.ToObject<ChartData>(chartText.text);
+
             // 再度jsonデータにシリアライズ
-            var json = LitJson.JsonMapper.ToJson(chart);
-            
+            var json = JsonMapper.ToJson(chart);
+
             // ログ出力
             Debug.Log(json);
         }
-
     }
 }
