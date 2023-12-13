5d4ddd26133d84219541758be283cfb0433f2774
FraMari495 <mirochacha_u_u_1106@yahoo.co.jp>
2023-12-14 04:42:00 +0900

    [modify] リフォーマット

--- Assets/Scripts/InputSystemUtility/EnhancedTouchManager.cs
+++ Assets/Scripts/InputSystemUtility/EnhancedTouchManager.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using UniRx;
 using UnityEngine.InputSystem.EnhancedTouch;
 using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch.Touch;
@@ -8,42 +7,33 @@ namespace InputSystemUtility
 {
     public class EnhancedTouchManager : MonoSingleton<EnhancedTouchManager>
     {
+        private readonly Subject<Finger> onFingerUp = new();
+
         /********************* 通知用プロパティ ***********************/
         /// <summary>
-        /// 指が触れたことを通知
+        ///     指が触れたことを通知
         /// </summary>
         public IObservable<Finger> OnFingerDown => onFingerDown;
+
         private Subject<Finger> onFingerDown { get; } = new();
 
         /// <summary>
-        /// 指が動かされたことを通知
+        ///     指が動かされたことを通知
         /// </summary>
         public IObservable<Finger> OnFingerMove => onFingerMove;
+
         private Subject<Finger> onFingerMove { get; } = new();
 
         /// <summary>
-        /// 指が離されたことを通知
+        ///     指が離されたことを通知
         /// </summary>
         public IObservable<Finger> OnFingerUp => onFingerUp;
-        private readonly Subject<Finger> onFingerUp = new();
         /*************************************************************/
 
-        
-        
-        
-        
-        
-        /******** EnhancedTouchのTouchイベントに追加するデリゲート *********/
-        private void onDownAction(Finger f) => onFingerDown.OnNext(f);
-        private void onMoveAction(Finger f) => onFingerMove.OnNext(f);
-        private void onUpAction(Finger f) => onFingerUp.OnNext(f);
-        /*************************************************************/
 
-        
-        
         /// <summary>
-        /// EnhancedTouchを有効にし、
-        /// Touchイベントに通知を発行するアクションを登録
+        ///     EnhancedTouchを有効にし、
+        ///     Touchイベントに通知を発行するアクションを登録
         /// </summary>
         private void Start()
         {
@@ -52,10 +42,28 @@ namespace InputSystemUtility
             Touch.onFingerMove += onMoveAction;
             Touch.onFingerUp += onUpAction;
         }
+        /*************************************************************/
+
+
+        /******** EnhancedTouchのTouchイベントに追加するデリゲート *********/
+        private void onDownAction(Finger f)
+        {
+            onFingerDown.OnNext(f);
+        }
+
+        private void onMoveAction(Finger f)
+        {
+            onFingerMove.OnNext(f);
+        }
+
+        private void onUpAction(Finger f)
+        {
+            onFingerUp.OnNext(f);
+        }
 
         /// <summary>
-        /// EnhancedTouchを無効にし、
-        /// Touchイベントに通知を発行するアクションを除去
+        ///     EnhancedTouchを無効にし、
+        ///     Touchイベントに通知を発行するアクションを除去
         /// </summary>
         protected override void BeforeOnDestroy()
         {
@@ -63,7 +71,6 @@ namespace InputSystemUtility
             EnhancedTouch.onFingerMove -= onMoveAction;
             EnhancedTouch.onFingerUp -= onUpAction;
             EnhancedTouchSupport.Disable();
-            
         }
     }
 }
--- Assets/Scripts/InputSystemUtility/InputReceiver.cs
+++ Assets/Scripts/InputSystemUtility/InputReceiver.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 using UnityEngine.InputSystem.EnhancedTouch;
-using UniRx;
 
 namespace InputSystemUtility
 {
     /// <summary>
-    /// このクラスを継承したオブジェクトにユーザーのインプットがあった時
-    /// コールバックメソッドが呼ばれる
+    ///     このクラスを継承したオブジェクトにユーザーのインプットがあった時
+    ///     コールバックメソッドが呼ばれる
     /// </summary>
     public abstract class InputReceiver : MonoBehaviour
     {
-        
         private void Start()
         {
             // インプットイベントを受け取る
@@ -22,23 +19,21 @@ namespace InputSystemUtility
         }
 
         /// <summary>
-        /// 指が触れた時に呼ばれるコールバックメソッド
+        ///     指が触れた時に呼ばれるコールバックメソッド
         /// </summary>
         /// <param name="finger"></param>
         protected abstract void OnFingerDown(Finger finger);
-        
+
         /// <summary>
-        /// 指が動いた時に呼ばれるコールバックメソッド
+        ///     指が動いた時に呼ばれるコールバックメソッド
         /// </summary>
         /// <param name="finger"></param>
         protected abstract void OnFingerMove(Finger finger);
-        
+
         /// <summary>
-        /// 指が離された時に呼ばれるコールバックメソッド
+        ///     指が離された時に呼ばれるコールバックメソッド
         /// </summary>
         /// <param name="finger"></param>
         protected abstract void OnFingerUp(Finger finger);
-
-
     }
 }
