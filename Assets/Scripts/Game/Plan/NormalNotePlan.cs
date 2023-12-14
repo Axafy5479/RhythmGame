98b4c498d0477aa0f442b2befeb2f0fabc1f16e4
FraMari495 <mirochacha_u_u_1106@yahoo.co.jp>
2023-12-14 10:44:43 +0900

    [add] data to plan converter

---
+++ Assets/Scripts/Game/Plan/NormalNotePlan.cs
@@ -0,0 +1,9 @@
+namespace Game.Plan
+{
+    public class NormalNotePlan : NotePlanBase
+    {
+        public NormalNotePlan(int noteId, int beatTime, float bpm, int block) : base(noteId, beatTime, block, bpm)
+        {
+        }
+    }
+}
