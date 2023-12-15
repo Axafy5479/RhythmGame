f95855a3ad27eb2540f1f1c0728255506ff64633
FraMari495 <mirochacha_u_u_1106@yahoo.co.jp>
2023-12-16 02:13:40 +0900

    [modify] notepool transform parent name

--- Assets/Scripts/Game/NoteController/Pool/NotePool.cs
+++ Assets/Scripts/Game/NoteController/Pool/NotePool.cs
@@ -3,23 +3,26 @@ using UniRx;
 using UniRx.Toolkit;
 using UnityEngine;
 
-public class NotePool : ObjectPool<NoteController>
+namespace Game
 {
-    public NotePool(int number)
+    public class NotePool : ObjectPool<NoteController>
     {
-        PreloadAsync(number, 10).Subscribe();
-        NoteParent = new GameObject("_noteroot").transform;
-    }
+        public NotePool(int number)
+        {
+            PreloadAsync(number, 10).Subscribe();
+            NoteParent = new GameObject("_notepool").transform;
+        }
 
-    private Transform NoteParent { get; }
+        private Transform NoteParent { get; }
 
-    /// <summary>
-    ///     インスタンスの生成
-    /// </summary>
-    /// <returns></returns>
-    protected override NoteController CreateInstance()
-    {
-        var prefab = Resources.Load<NoteController>("Game/note");
-        return Object.Instantiate(prefab, NoteParent);
+        /// <summary>
+        ///     インスタンスの生成
+        /// </summary>
+        /// <returns></returns>
+        protected override NoteController CreateInstance()
+        {
+            var prefab = Resources.Load<NoteController>("Game/note");
+            return Object.Instantiate(prefab, NoteParent);
+        }
     }
 }
--- Assets/Scripts/Game/NoteGenerator/NoteGenerator.cs
+++ Assets/Scripts/Game/NoteGenerator/NoteGenerator.cs
@@ -6,19 +6,19 @@ using UnityEngine;
 namespace Game
 {
     /// <summary>
-    /// LaunchTimeManagerの通知を受けて、ノーツを生成するクラス
+    ///     LaunchTimeManagerの通知を受けて、ノーツを生成するクラス
     /// </summary>
     [RequireComponent(typeof(LaunchTimeManager))]
     public class NoteGenerator : MonoBehaviour, INeedInitializing
     {
         /// <summary>
-        /// NoteControllerのオブジェクトプール
+        ///     NoteControllerのオブジェクトプール
         /// </summary>
         private NotePool noteControllerPool;
-        
+
         /// <summary>
-        /// idとNotePlanの辞書
-        /// (LaunchTimeManagerからidが通知されるため、これに紐づくPlanを用いてノーツを初期化する)
+        ///     idとNotePlanの辞書
+        ///     (LaunchTimeManagerからidが通知されるため、これに紐づくPlanを用いてノーツを初期化する)
         /// </summary>
         private Dictionary<int, NotePlan> NotePlanMap { get; set; }
 
@@ -32,37 +32,37 @@ namespace Game
         {
             // id_Plan の辞書を作成
             NotePlanMap = gameInfo.GetPlanMap();
-            
+
             // LaunchTimeManagerの通知を購読
             var launchTime = GetComponent<LaunchTimeManager>();
-            launchTime.OnLaunchTime.Subscribe(noteId=>Launch(NotePlanMap[noteId]));
+            launchTime.OnLaunchTime.Subscribe(noteId => Launch(NotePlanMap[noteId]));
         }
 
         /// <summary>
-        /// LaunchTimeManagerの通知を受けて実行するメソッド
+        ///     LaunchTimeManagerの通知を受けて実行するメソッド
         /// </summary>
         /// <param name="plan"></param>
         private void Launch(NotePlan plan)
         {
             // プールからNoteControllerを取り出す
             var controller = noteControllerPool.Rent();
-                
+
             // NoteControllerにPlanを設定
             controller.Activated(plan);
-                
+
             // Spawn位置に設定
             controller.Trn.position = Field.Instance.GetSpawnPos(plan.Block);
 
             // 発射
             controller.Launch();
-            
+
 #if UNITY_EDITOR
             // デバッグしやすいように、オブジェクトの名前としてPlanを設定
             controller.name = $"{plan}";
 #endif
-            
+
             // 子ノーツがあれば、それも射出する
-            if(plan.ChildNote is { } childPlan) Launch(childPlan);
+            if (plan.ChildNote is { } childPlan) Launch(childPlan);
         }
     }
 }
